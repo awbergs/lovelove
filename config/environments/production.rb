b3fbe1e1bab2e9e7459ee6e62c91cccf414d9138
Austin Bergstrom <awbergs@awbergs-VirtualBox.(none)>
2013-02-13 15:01:39 -0500

    modified production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,17 +8,24 @@ Love::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # # Disable Rails's static asset server (Apache or nginx will already do this)
+  # config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # # Compress JavaScripts and CSS
+  # config.assets.compress = false
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # # Don't fallback to assets pipeline if a precompiled asset is missed
+  # config.assets.compile = false
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  # # Generate digests for assets URLs
+  # config.assets.digest = true
+
+  
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
